d0f15cb973ff51408e543240737e914c9c662561
Hrishirt <hrishishahh@gmail.com>
2025-08-25 20:21:18 -0600

    Added SQL database and configured delete to work with it

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for
 from markupsafe import escape
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__) 
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///notes.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
 print(app.url_map)
 '''
 @app.route('/') 
@@ -9,6 +16,17 @@ def helloworld():
     notes = ["Study MathAcademy", "Finish basic notes app", "Eat Dinner"]
     return render_template('notes.html', notes=notes)
 '''
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    
+    def __repr__(self):
+        return f'<Note {self.id}: {self.content[:20]}...>'
+    
+with app.app_context():
+    db.create_all()
+
 @app.route("/hello")
 def hello():
     name = request.args.get("name", "Flask")
@@ -21,19 +39,25 @@ def noteform():
 '''
 
 notes = [] 
+
+
 @app.route('/', methods =['POST', 'GET'])
 def noteTake():
     if request.method == 'POST':
         note = request.form['text']
         if note:
             notes.append(note)
+            db.session.add(Note(content=note))
+            db.session.commit()
         return redirect(url_for('noteTake'))
-    return render_template('form.html', notes=notes)
+    allNotes = Note.query.all()
+    print(allNotes)
+    return render_template('form.html', notes=allNotes)
 
 @app.route('/delete/<int:i>', methods=['POST'], endpoint="delete_note")
 def delete_note(i):
-    if 0 <= i < len(notes):
-        notes.pop(i)
+    Note.query.filter_by(id=i).delete()
+    db.session.commit()
     return redirect(url_for('noteTake'))
 
 @app.route('/edit/<int:i>', methods=['GET', 'POST'], endpoint="edit_note")
